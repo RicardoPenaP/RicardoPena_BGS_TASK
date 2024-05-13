83f23617545a1908bbf6816d33cba0ad9255661d
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-13 16:47:05 -0300

    Updated PlayerController class
    Defined view and model fields

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Player/PlayerController.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Player/PlayerController.cs
@@ -4,6 +4,10 @@ namespace Gameplay.Entities.Player
 {
     public class PlayerController : MonoBehaviour
     {
-        
+        [Header("Player Controller")]
+        [Header("References")]
+        [SerializeField] private PlayerView playerView;
+        [SerializeField] private PlayerModel playerModel;
+
     }
 }
