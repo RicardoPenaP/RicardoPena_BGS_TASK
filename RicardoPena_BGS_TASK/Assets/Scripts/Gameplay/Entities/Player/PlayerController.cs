8166e44039d31203350ab9230125375c7edb3f83
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-13 16:58:50 -0300

    Updated PlayerController class
    Defined entity movement field and implemented its initialization and deinitialization

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Player/PlayerController.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using Gameplay.Entities.Common.EntityMovement;
 using UnityEngine;
 
 namespace Gameplay.Entities.Player
@@ -9,5 +10,26 @@ namespace Gameplay.Entities.Player
         [SerializeField] private PlayerView playerView;
         [SerializeField] private PlayerModel playerModel;
 
+        private EntityMovementController entityMovementController;
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void OnDestroy()
+        {
+            Deinit();
+        }
+
+        private void Init()
+        {
+            entityMovementController = new EntityMovementController(playerView, playerModel);
+        }
+
+        private void Deinit()
+        {
+            entityMovementController.Dispose();
+        }
     }
 }
