35a55e0127071d52895e776cb38a6c2af7cbceda
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-13 16:54:21 -0300

    Updated EnitityMovementController class
    Defined new class fields and implemented IDisposable interface

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Common/EntityMovement/EntityMovementController.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Common/EntityMovement/EntityMovementController.cs
@@ -1,7 +1,34 @@
+using System;
+
 namespace Gameplay.Entities.Common.EntityMovement
 {
-    public class EntityMovementController 
+    public class EntityMovementController  : IDisposable
     {
+        private readonly IEntityMovementView entityMovementView;
+        private readonly IEntityMovementModel entityMovementModel;
+
+        public EntityMovementController(IEntityMovementView entityMovementView, IEntityMovementModel entityMovementModel)
+        {
+            this.entityMovementView = entityMovementView;
+            this.entityMovementModel = entityMovementModel;
+            Init();
+        }
+
+        public void Dispose()
+        {
+            Deinit();
+        }
+
+        private void Init()
+        {
+
+        }
+
+        private void Deinit()
+        {
+
+        }
+
         
     }
 }
