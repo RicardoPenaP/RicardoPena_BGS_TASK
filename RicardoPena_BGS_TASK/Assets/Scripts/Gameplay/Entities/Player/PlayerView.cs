207c29055279e3345162e270e7f96f284f51301e
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-13 17:06:55 -0300

    Updated PlayerView class
    Implemented movement input detection and notification logic

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Player/PlayerView.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Player/PlayerView.cs
@@ -1,10 +1,37 @@
 ﻿using Gameplay.Entities.Common.EntityMovement;
+using Gameplay.Input;
+using System;
 using UnityEngine;
 
 namespace Gameplay.Entities.Player
 {
     public class PlayerView : MonoBehaviour, IEntityMovementView
     {
+        [Header("Player View")]
+        [Header("References")]
+        [SerializeField] private InputReader inputReader;
+
+        public event Action<Vector2> OnMoveInputDetected;
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void OnDestroy()
+        {
+            Deinit();
+        }
+
+        private void Init()
+        {
+            inputReader.OnMoveInputUpdated += (rawInput) => OnMoveInputDetected?.Invoke(rawInput);
+        }
+
+        private void Deinit()
+        {
+            inputReader.OnMoveInputUpdated -= (rawInput) => OnMoveInputDetected?.Invoke(rawInput);
+        }
 
     }
 }
