e5d1291480b42e6e60cad78a64dba0c997c9ec37
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-13 17:10:01 -0300

    Updated Player Model class
    Implemented movement related logic

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Player/PlayerModel.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Player/PlayerModel.cs
@@ -5,7 +5,18 @@ namespace Gameplay.Entities.Player
 {
     public class PlayerModel : MonoBehaviour, IEntityMovementModel
     {
+        [Header("Player Model")]
+        [Header("References")]
+        [SerializeField] private Rigidbody2D playerRigidbody;
 
-        
+        public void MoveTowards(Vector2 movementDirection)
+        {
+            playerRigidbody.velocity = movementDirection * 6f;          //Change the value with movement settings value
+        }
+
+        public void StopMovement()
+        {
+            MoveTowards(Vector2.zero);
+        }
     }
 }
