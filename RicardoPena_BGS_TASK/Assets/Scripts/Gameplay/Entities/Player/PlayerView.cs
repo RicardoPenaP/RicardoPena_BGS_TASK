e3b4d777c3b359cdb08bcd9f0c5662d8a58fbe91
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-13 17:42:11 -0300

    Updated PlayerView class
    Implemented changing looking direction logic

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Player/PlayerView.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Player/PlayerView.cs
@@ -10,6 +10,7 @@ namespace Gameplay.Entities.Player
         [Header("Player View")]
         [Header("References")]
         [SerializeField] private InputReader inputReader;
+        [SerializeField] private Transform spriteRendererTransform;
 
         public event Action<Vector2> OnMoveInputDetected;
 
@@ -25,13 +26,37 @@ namespace Gameplay.Entities.Player
 
         private void Init()
         {
-            inputReader.OnMoveInputUpdated += (rawInput) => OnMoveInputDetected?.Invoke(rawInput);
+            inputReader.OnMoveInputUpdated += InputReader_OnMoveInputUpdated;           
         }
 
         private void Deinit()
         {
-            inputReader.OnMoveInputUpdated -= (rawInput) => OnMoveInputDetected?.Invoke(rawInput);
+            inputReader.OnMoveInputUpdated -= InputReader_OnMoveInputUpdated;
         }
 
+        private void InputReader_OnMoveInputUpdated(Vector2 rawInput)
+        {
+            SetLookingDirection(rawInput.x);
+            OnMoveInputDetected?.Invoke(rawInput);
+        }
+
+        private void SetLookingDirection(float direction)
+        {
+            if (Mathf.Abs(direction) <= Mathf.Epsilon)
+            {
+                return;
+            }
+
+            float normalizedDirection = direction > Mathf.Epsilon ? 1f : -1f;
+
+            if (spriteRendererTransform.localScale.x.Equals(normalizedDirection))
+            {
+                return;
+            }
+
+            Vector3 newLocalScale = spriteRendererTransform.localScale;
+            newLocalScale.x = normalizedDirection;
+            spriteRendererTransform.localScale = newLocalScale;
+        }
     }
 }
