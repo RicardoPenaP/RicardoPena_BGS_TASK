9808611f859606651d924562001d141534487667
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-13 17:27:45 -0300

    Created EntityMovementSettings class

---
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Common/EntityMovement/EntityMovementSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Entities.Common.EntityMovement
+{
+    [CreateAssetMenu(fileName = "NewEntityMovementSettings", menuName = "Gameplay/Entities/Common/EntityMovement/Entity Movement Settings")]
+    public class EntityMovementSettings : ScriptableObject
+    {
+        [Header("Entity Movement Settings")]
+        [SerializeField] private float movementSpeed = 6f;
+
+        public float MovementSpeed => movementSpeed;
+    }
+}
