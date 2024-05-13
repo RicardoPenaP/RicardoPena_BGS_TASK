091c6dc797c109a795e8aa1ba63dbc34452a0f98
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-13 16:27:14 -0300

    Created InputReader class

---
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Input/InputReader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static Gameplay.Input.Controls;
+
+namespace Gameplay.Input
+{
+    [CreateAssetMenu(fileName = "NewInputReader", menuName = "Gameplay/Input/Input Reader")]
+    public class InputReader : ScriptableObject, IPlayerActions
+    {
+        public event Action<bool> OnInteractInputUpdated;
+        public event Action<Vector2> OnMoveInputUpdated;
+
+        private Controls controls;
+
+        private void OnEnable()
+        {
+            if (controls is not null)
+            {
+                return;
+            }
+
+            controls = new Controls();
+            controls.Player.SetCallbacks(this);
+            controls.Player.Enable();
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            bool state = false;
+            switch (context.phase)
+            {               
+                case InputActionPhase.Started:
+                    state = true;
+                    break;                
+                case InputActionPhase.Canceled:
+                    state = false;
+                    break;
+                default:
+                    break;
+            }
+            OnInteractInputUpdated?.Invoke(state);
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            OnMoveInputUpdated?.Invoke(context.ReadValue<Vector2>());
+        }
+    }
+}
