7596c52318308f1cb9e3dc9fe6d8a4e718a323a0
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-14 18:27:18 -0300

    Updated Shopkeeper class
    Implemented IInteractable interface

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Shopkeeper/Shopkeeper.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Shopkeeper/Shopkeeper.cs
@@ -1,21 +1,35 @@
+using Gameplay.Entities.Common.EntityInteractability;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Gameplay.Entities.Shopkeeper
 {
-    public class Shopkeeper : MonoBehaviour
+    public class Shopkeeper : MonoBehaviour, IInteractable
     {
-        // Start is called before the first frame update
-        void Start()
+        [Header("Shopkeeper")]
+        [Header("References")]
+        [SerializeField] private ShopkeeperController shopkeeperController;
+
+        public Vector2 GetInteractablePosition()
         {
-        
+            return transform.position;
         }
 
-        // Update is called once per frame
-        void Update()
+        public void HideInteractabilityFeedback()
         {
-        
+            throw new System.NotImplementedException();
         }
+
+        public void Interact()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ShowInteractabilityFeedback()
+        {
+            throw new System.NotImplementedException();
+        }
+
     }
 }
