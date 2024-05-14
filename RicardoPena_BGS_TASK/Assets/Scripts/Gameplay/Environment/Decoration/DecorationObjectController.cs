930a52f16b3be003568b402077d8215144a4e248
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-14 16:56:24 -0300

    Updated DecorationObjectController class
    Defined view and model fields

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Environment/Decoration/DecorationObjectController.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Environment/Decoration/DecorationObjectController.cs
@@ -1,21 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Gameplay.Environment.Decoration
 {
     public class DecorationObjectController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+        [Header("Decoration Object Controller")]
+        [Header("References")]
+        [SerializeField] private DecorationObjectView decorationObjectView;
+        [SerializeField] private DecorationObjectModel decorationObjectModel;
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+
     }
 }
