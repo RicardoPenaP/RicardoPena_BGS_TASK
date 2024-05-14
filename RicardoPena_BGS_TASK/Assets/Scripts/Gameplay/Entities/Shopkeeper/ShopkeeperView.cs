694ef76969c6474badebfc9353efc09584dbe054
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-14 18:28:56 -0300

    Updated asmdef asset

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Shopkeeper/ShopkeeperView.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Shopkeeper/ShopkeeperView.cs
@@ -1,21 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Gameplay.Entities.Shopkeeper
 {
     public class ShopkeeperView : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
         
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
