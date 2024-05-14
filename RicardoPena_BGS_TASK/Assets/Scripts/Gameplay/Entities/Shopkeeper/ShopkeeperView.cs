6603c7fdcbbe7ddc5702c835b05e3e8d4862c307
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-14 18:31:05 -0300

    Updated ShopkeeperView class
    Implemented toggle method

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Shopkeeper/ShopkeeperView.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Shopkeeper/ShopkeeperView.cs
@@ -1,9 +1,22 @@
+using TMPro;
 using UnityEngine;
 
 namespace Gameplay.Entities.Shopkeeper
 {
     public class ShopkeeperView : MonoBehaviour
     {
-        
+        [Header("Shopkeeper View")]
+        [Header("References")]
+        [SerializeField] private TextMeshProUGUI interactionText;
+
+        private void Awake()
+        {
+            ToggleInteractionText(false);
+        }
+
+        public void ToggleInteractionText(bool state)
+        {
+            interactionText.gameObject.SetActive(state);
+        }
     }
 }
