1d6aaa2f8d172b04eb6e93dcef22c84416fc51aa
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-14 15:27:30 -0300

    Updated IInteractabilityView interface
    Defined interface event

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Common/EntityInteractability/IInteractabilityView.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Common/EntityInteractability/IInteractabilityView.cs
@@ -5,5 +5,6 @@ namespace Gameplay.Entities.Common.EntityInteractability
     public interface IInteractabilityView 
     {
         public event Action<bool> OnInteractInputUpdated;
+        public event Action<IInteractable> OnInteractableEntityDetected;
     }
 }
