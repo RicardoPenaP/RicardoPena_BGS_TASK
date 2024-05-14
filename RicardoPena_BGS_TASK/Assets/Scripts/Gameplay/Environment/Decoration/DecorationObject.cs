177b6a77c1d13eb2c4529d73956aabe604560958
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-14 16:13:49 -0300

    Updated DecorationObject class
    Implemented IInteractable interface

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Environment/Decoration/DecorationObject.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Environment/Decoration/DecorationObject.cs
@@ -1,11 +1,32 @@
+using Gameplay.Entities.Common.EntityInteractability;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Gameplay.Environment.Decoration
 {
-    public class DecorationObject : MonoBehaviour
+    public class DecorationObject : MonoBehaviour, IInteractable
     {
-        
+        public Vector2 GetInteractablePosition()
+        {
+            return transform.position;
+        }
+
+        public void HideInteractabilityFeedback()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ShowInteractabilityFeedback()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Interact()
+        {
+            //Testing
+            Destroy(gameObject);
+        }
+
     }
 }
