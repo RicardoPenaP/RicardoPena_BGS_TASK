791d02857de289172670a4cabd8ec8de3cc2b39b
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-14 19:52:03 -0300

    Created InteractableSettings class

---
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Common/EntityInteractability/InteractableSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Entities.Common.EntityInteractability
+{
+    [CreateAssetMenu(fileName = "NewInteractableSettings", menuName = "Gameplay/Entities/Common/EntityInteractability/Interactable Settings")]
+    public class InteractableSettings : ScriptableObject
+    {
+        [Header("Interactable Settings")]
+        [SerializeField, Min(0f)] private float interactionTime = 0f;
+
+        public float InteractionTime => interactionTime;
+    }
+}
