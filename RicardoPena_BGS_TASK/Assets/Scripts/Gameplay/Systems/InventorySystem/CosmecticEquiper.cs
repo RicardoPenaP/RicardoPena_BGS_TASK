53a363612f0c21ce9f1de6b662d5cb39b8843737
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 09:43:28 -0300

    Updated ComecticEquiper class
    Implemented setter method

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/CosmecticEquiper.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/CosmecticEquiper.cs
@@ -1,3 +1,4 @@
+using Gameplay.Items;
 using UnityEngine;
 
 namespace Gameplay.Systems.InventorySystem
@@ -21,5 +22,59 @@ namespace Gameplay.Systems.InventorySystem
         [SerializeField] private SpriteRenderer sleeveR;
         [SerializeField] private SpriteRenderer torso;
         
+        public void SetCosmetic(IEquipable equipableItem)
+        {
+            foreach (ItemPart itemPart in equipableItem.ItemParts)
+            {
+                SpriteRenderer targetSpriteRenderer = null;
+                switch (itemPart.PartId)
+                {
+                    case "ArmL":
+                        targetSpriteRenderer = armL;
+                        break;
+                    case "ArmR":
+                        targetSpriteRenderer = armR;
+                        break;
+                    case "Finger":
+                        targetSpriteRenderer = finger;
+                        break;
+                    case "ForearmL":
+                        targetSpriteRenderer = forearmL;
+                        break;
+                    case "ForearmR":
+                        targetSpriteRenderer = forearmR;
+                        break;
+                    case "HandL":
+                        targetSpriteRenderer = handL;
+                        break;
+                    case "HandR":
+                        targetSpriteRenderer = handR;
+                        break;
+                    case "Leg":
+                        targetSpriteRenderer = legL;
+                        targetSpriteRenderer.sprite = itemPart.PartSprite;
+                        targetSpriteRenderer = legR;
+                        break;
+                    case "Pelvis":
+                        targetSpriteRenderer = pelvis;
+                        break;
+                    case "Shin":
+                        targetSpriteRenderer = shinL;
+                        targetSpriteRenderer.sprite = itemPart.PartSprite;
+                        targetSpriteRenderer = shinR; 
+                        break;
+                    case "SleeveR":
+                        targetSpriteRenderer = sleeveR;
+                        break;
+                    case "Torso":
+                        targetSpriteRenderer = torso;
+                        break;
+                    default:
+                        break;
+                }
+
+                targetSpriteRenderer.sprite = itemPart.PartSprite;
+            }
+        }
     }
 }
