61bd3d3fb88099e89e350c8170436404a2f98830
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 00:37:54 -0300

    Updated UnequipableItem class
    Implemented IStackable interface

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Items/UnequipableItem.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Items/UnequipableItem.cs
@@ -3,7 +3,7 @@
 namespace Gameplay.Items
 {
     [CreateAssetMenu(fileName = "NewUnequipableItem", menuName = "Gameplay/Items/Unequipable Item")]
-    public class UnequipableItem : Item
+    public class UnequipableItem : Item, IStackable
     {
         [SerializeField] private bool isStackable = false;
         [SerializeField] private int maxStackAmount = 100;
