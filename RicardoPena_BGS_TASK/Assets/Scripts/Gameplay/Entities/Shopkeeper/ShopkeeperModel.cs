de20994404fedbe78bb596e99c6845f7c311415a
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 06:27:09 -0300

    Updated ShopkeeperModel class
    Defined class fields

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Shopkeeper/ShopkeeperModel.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Shopkeeper/ShopkeeperModel.cs
@@ -1,5 +1,7 @@
+using Gameplay.Items;
 using Gameplay.Systems.ShopSystem;
 using Gameplay.Systems.ShopSystem.Common;
+using System;
 using UnityEngine;
 
 namespace Gameplay.Entities.Shopkeeper
@@ -8,7 +10,18 @@ namespace Gameplay.Entities.Shopkeeper
     {
         [Header("Shop Model")]
         [Header("References")]
-        [SerializeField] private item
+        [SerializeField] private Item[] sellableItems;
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            throw new NotImplementedException();
+        }
+
         public ShopSlot[] GetShopSlots()
         {
             throw new System.NotImplementedException();
