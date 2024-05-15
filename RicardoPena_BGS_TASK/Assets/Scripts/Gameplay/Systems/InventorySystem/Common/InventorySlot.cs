dd32284a8695dad963aa179da8eb734d372de549
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 01:07:55 -0300

    Updated InventorySlop prefab

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/Common/InventorySlot.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/Common/InventorySlot.cs
@@ -1,9 +1,16 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Gameplay.Systems.InventorySystem.Common
 {
     public class InventorySlot : MonoBehaviour
     {
-        
+        [Header("Inventory Slot")]
+        [Header("References")]
+        [SerializeField] private Image icon;
+        [SerializeField] private Image frame;
+        [SerializeField] private TextMeshProUGUI stackText;
+
     }
 }
