e05fdc210b2fc52cb1ef9605ce425d9eda44e09b
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 00:17:22 -0300

    Updated InventoryModel class
    Defined items slots field

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/InventoryModel.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/InventoryModel.cs
@@ -4,6 +4,8 @@ namespace Gameplay.Systems.InventorySystem
 {
     public class InventoryModel : MonoBehaviour
     {
-
+        [Header("Inventory Model")]
+        [Header("References")]
+        [SerializeField] private GameObject[] itemSlots;
     }
 }
