1816605a917f7ea908ef5d3520fed31fcb74d8a5
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 06:25:55 -0300

    Updated asmdef asset

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Shopkeeper/ShopkeeperModel.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Entities/Shopkeeper/ShopkeeperModel.cs
@@ -1,9 +1,17 @@
+using Gameplay.Systems.ShopSystem;
+using Gameplay.Systems.ShopSystem.Common;
 using UnityEngine;
 
 namespace Gameplay.Entities.Shopkeeper
 {
-    public class ShopkeeperModel : MonoBehaviour
+    public class ShopkeeperModel : MonoBehaviour, IShopModel
     {
-        
+        [Header("Shop Model")]
+        [Header("References")]
+        [SerializeField] private item
+        public ShopSlot[] GetShopSlots()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
