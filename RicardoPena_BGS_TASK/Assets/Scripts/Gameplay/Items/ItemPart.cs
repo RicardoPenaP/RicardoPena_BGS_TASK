8ddd28a511e1c46985a7e6cbb022acb43920d6a3
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 09:04:18 -0300

    Created ItemPart class

---
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Items/ItemPart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Items
+{
+    [CreateAssetMenu(fileName = "NewItemPart", menuName = "Gameplay/Items/Item Part")]
+    public class ItemPart : ScriptableObject
+    {
+        [Header("Item Part")]
+        [Header("References")]
+        [SerializeField] private string partId;
+        [SerializeField] private Sprite partSprite;
+
+        public string PartId => partId;
+        public Sprite PartSprite => partSprite;
+    }
+}
