2f806ecd113efc8e434ae024124dc70e96c2763c
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 01:47:06 -0300

    Updated InvetoryModel class
    Implemented add item logic

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/InventoryModel.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/InventoryModel.cs
@@ -1,4 +1,5 @@
-﻿using Gameplay.Systems.InventorySystem.Common;
+﻿using Gameplay.Items;
+using Gameplay.Systems.InventorySystem.Common;
 using UnityEngine;
 
 namespace Gameplay.Systems.InventorySystem
@@ -8,5 +9,51 @@ namespace Gameplay.Systems.InventorySystem
         [Header("Inventory Model")]
         [Header("References")]
         [SerializeField] private InventorySlot[] invetorySlots;
+
+        public bool TryToAddItem(Item item, int amount = 1)
+        {
+            if (TryStackItem(item,amount))
+            {
+                return true;
+            }
+
+            if (TryToAddToNewSlot(item,amount))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool TryStackItem(Item item, int amount)
+        {
+            foreach (InventorySlot inventorySlot in invetorySlots)
+            {
+                if (inventorySlot.CurrentItem.ItemName.Equals(item.ItemName))
+                {
+                    if (inventorySlot.TryToStackItem(item, amount))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private bool TryToAddToNewSlot(Item item, int amount)
+        {
+            foreach (InventorySlot inventorySlot in invetorySlots)
+            {
+                if (inventorySlot.CurrentItem is not null)
+                {
+                    continue;
+                }
+                inventorySlot.SetCurrentItem(item, amount);
+                return true;
+            }
+            return false;
+        }
+        
     }
 }
