c894a4a0c94834cf188c4d05c25de7f79f72f32b
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 09:06:58 -0300

    Updated IEquipable interface
    Defined property

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Items/EquipableItem.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Items/EquipableItem.cs
@@ -5,6 +5,8 @@ namespace Gameplay.Items
     [CreateAssetMenu(fileName = "NewEquipableItem", menuName = "Gameplay/Items/Equipable Item")]
     public class EquipableItem : Item, IEquipable
     {
-        [SerializeField] private Sprite itemSprite;
+        [SerializeField] private ItemPart[] itemParts;
+
+        public ItemPart[] ItemParts => itemParts;
     }
 }
--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Items/IEquipable.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Items/IEquipable.cs
@@ -2,6 +2,6 @@ namespace Gameplay.Items
 {
     public interface IEquipable 
     {
-        
+        public ItemPart[] ItemParts { get;}
     }
 }
