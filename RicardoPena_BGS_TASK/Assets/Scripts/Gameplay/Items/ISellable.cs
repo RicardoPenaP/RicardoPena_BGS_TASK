8d947b27deae408b4bfe8cdf56feed128a0fd67c
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 00:36:17 -0300

    Created ISellable interface

---
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Items/ISellable.cs
@@ -0,0 +1,7 @@
+﻿namespace Gameplay.Items
+{
+    public interface ISellable 
+    {
+
+    }
+}
--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Items/UnequipableItem.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Items/UnequipableItem.cs
@@ -5,6 +5,7 @@ namespace Gameplay.Items
     [CreateAssetMenu(fileName = "NewUnequipableItem", menuName = "Gameplay/Items/Unequipable Item")]
     public class UnequipableItem : Item
     {
-
+        [SerializeField] private bool isStackable = false;
+        [SerializeField] private int maxStackAmount = 100;
     }
 }
