705cbc04bd9b78ec96efb5a1002a50968230513a
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 09:35:30 -0300

    Updated Player prefab
    Added component

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/CosmecticEquiper.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/CosmecticEquiper.cs
@@ -6,6 +6,20 @@ namespace Gameplay.Systems.InventorySystem
     {
         [Header("Comestic Equiper")]
         [Header("References")]
-        [SerializeField] private SpriteRenderer[] bodyParts;
+        [SerializeField] private SpriteRenderer armL;
+        [SerializeField] private SpriteRenderer armR;
+        [SerializeField] private SpriteRenderer finger;
+        [SerializeField] private SpriteRenderer forearmL;
+        [SerializeField] private SpriteRenderer forearmR;
+        [SerializeField] private SpriteRenderer handL;
+        [SerializeField] private SpriteRenderer handR;
+        [SerializeField] private SpriteRenderer legL;
+        [SerializeField] private SpriteRenderer legR;
+        [SerializeField] private SpriteRenderer pelvis;
+        [SerializeField] private SpriteRenderer shinL;
+        [SerializeField] private SpriteRenderer shinR;
+        [SerializeField] private SpriteRenderer sleeveR;
+        [SerializeField] private SpriteRenderer torso;
+        
     }
 }
