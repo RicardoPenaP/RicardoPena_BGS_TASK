9bb94ca24cda4be6816fdb7afe236f07d46b8464
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 06:39:40 -0300

    Updated asmdef asset

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/ShopSystem/Common/ShopSlot.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/ShopSystem/Common/ShopSlot.cs
@@ -2,6 +2,38 @@
 {
     public class ShopSlot 
     {
+        private Item currentItem;
+        private int itemAmount;
 
+        public Item CurrentItem => currentItem;
+        public int ItemAmount => itemAmount;
+
+        public InventorySlot()
+        {
+            currentItem = null;
+            itemAmount = 0;
+        }
+
+        public void SetCurrentItem(Item item, int itemAmount = 1)
+        {
+            currentItem = item;
+            this.itemAmount = itemAmount;
+        }
+
+        public bool TryToStackItem(Item item, int itemAmount)
+        {
+            if (currentItem is not IStackable)
+            {
+                return false;
+            }
+
+            if ((currentItem as IStackable).GetMaxStackAmount() < this.itemAmount + itemAmount)
+            {
+                return false;
+            }
+
+            this.itemAmount += itemAmount;
+            return true;
+        }
     }
 }
