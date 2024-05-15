7f3d051fd8137c151c78637366efd2cb9038c5ae
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 06:06:14 -0300

    Updated ShopSlotVisual prefab

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/ShopSystem/ShopView.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/ShopSystem/ShopView.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+﻿using Gameplay.Systems.ShopSystem.Common;
+using UnityEngine;
 
 namespace Gameplay.Systems.ShopSystem
 {
     public class ShopView : MonoBehaviour
     {
-
+        [Header("Shop View")]
+        [Header("References")]
+        [SerializeField] private ShopSlotVisual shopSlotVisualPrefab;
+        [SerializeField] private Transform slotLayout;
     }
 }
