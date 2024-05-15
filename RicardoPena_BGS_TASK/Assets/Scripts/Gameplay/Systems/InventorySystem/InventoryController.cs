b418b80c3b3ceb6205401521a9f5e9715d628a06
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-14 22:56:05 -0300

    Updated InvetoryController class
    Defined view and model fields

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/InventoryController.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/InventoryController.cs
@@ -4,7 +4,10 @@ namespace Gameplay.Systems.InventorySystem
 {
     public class InventoryController : MonoBehaviour
     {
-
+        [Header("Inventory Controller")]
+        [Header("References")]
+        [SerializeField] private InventoryView inventoryView;
+        [SerializeField] private InventoryModel inventoryModel;
     }
 }
 
