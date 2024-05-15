1e25a2fd307dc31ebb4ca3c1f3a8b18209710a5f
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 00:15:41 -0300

    Updated InventoryView class
    Implemented basic view logic

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/InventoryView.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/InventoryView.cs
@@ -1,9 +1,70 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Gameplay.Systems.InventorySystem
 {
     public class InventoryView : MonoBehaviour
     {
+        [Header("Inventory View")]
+        [Header("References")]
+        [SerializeField] private Button closeButton;
+        [SerializeField] private Button sellButton;
+        [SerializeField] private Button equipButton;
+        [SerializeField] private TextMeshProUGUI goldText;
 
+        public event Action OnCloseButtonPressed;
+        public event Action OnSellButtonPressed;
+        public event Action OnEquipButtonPressed;
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void OnDestroy()
+        {
+            Deinit();
+        }
+
+        private void Init()
+        {
+            closeButton.onClick.AddListener(CloseButton_OnClick);
+            sellButton.onClick.AddListener(SellButton_OnClick);
+            equipButton.onClick.AddListener(EquipButton_OnClick);
+        }
+
+        private void Deinit()
+        {
+            closeButton.onClick.RemoveListener(CloseButton_OnClick);
+            sellButton.onClick.RemoveListener(SellButton_OnClick);
+            equipButton.onClick.RemoveListener(EquipButton_OnClick);
+        }
+
+        private void CloseButton_OnClick()
+        {
+            OnCloseButtonPressed?.Invoke();
+        }
+
+        private void SellButton_OnClick()
+        {
+            OnSellButtonPressed?.Invoke();
+        }
+
+        private void EquipButton_OnClick()
+        {
+            OnEquipButtonPressed?.Invoke();
+        }
+
+        public void ToggleSellButton(bool state)
+        {
+            sellButton.gameObject.SetActive(state);
+        }
+
+        public void ToggleEquipButton(bool state)
+        {
+            equipButton.gameObject.SetActive(state);
+        }
     }
 }
