e5ddeb8fc792a0c7814a82ee874cdfa3507885e9
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 09:26:11 -0300

    Updated CosmeticEquiper class

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/CosmecticEquiper.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/CosmecticEquiper.cs
@@ -1,21 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Gameplay.Systems.InventorySystem
 {
     public class CosmecticEquiper : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+        [Header("Comestic Equiper")]
+        [Header("References")]
+        [SerializeField] private SpriteRenderer[] bodyParts;
     }
 }
