358b5c1c707903b289d2a26cfcd1655d5b9a8a9d
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-15 06:02:30 -0300

    Updated asmdef asset

---
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/ShopSystem/Common/ShopSlotVisual.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gameplay.Systems.ShopSystem.Common
+{
+    public class ShopSlotVisual : MonoBehaviour
+    {
+        [Header("Shop Slot Visual")]
+        [Header("References")]
+        [SerializeField] private Image icon;
+        [SerializeField] private Te
+    }
+}
