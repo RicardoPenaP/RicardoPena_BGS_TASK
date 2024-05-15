7c6ab805c894b16e10f981e1936e04b88910c16a
RicardoPenaP <ricardopena241197@gmail.com>
2024-05-14 22:58:48 -0300

    Updated InventorySystem class
    Implemented singleton pattern

--- RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/InventorySystem.cs
+++ RicardoPena_BGS_TASK/Assets/Scripts/Gameplay/Systems/InventorySystem/InventorySystem.cs
@@ -4,7 +4,29 @@ namespace Gameplay.Systems.InventorySystem
 {
     public class InventorySystem : MonoBehaviour
     {
-        
+        private static InventorySystem instance;
+        private static InventorySystem Instance => instance;
+
+        [Header("Inventory System")]
+        [Header("References")]
+        [SerializeField] private InventoryController inventoryController;
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            if (instance is not null && !instance.Equals(this))
+            {
+                Destroy(this);
+            }
+            else
+            {
+                instance = this;
+            }
+        }
     }
 }
 
